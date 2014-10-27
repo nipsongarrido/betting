2464cf66487cb2c8f43c761cd91f97b5fe017e04
Jorge Nipson Garrido Garrido <nipson.garrido02@gmail.com>
2014-10-27 01:07:29 -0800

    asignar valores a la vista segun el xml del api

--- app/controllers/EquiposController.php
+++ app/controllers/EquiposController.php
@@ -4,33 +4,63 @@ class EquiposController extends BaseController {
 
 	public function getLista()
 	{
-
-			$data["partidos"] = [
-				[
-					"fecha" => "2014-09-01",
-					"local" => "GB",
-					"visita" => "SF",
-					"img_local" => "http://prod.static.packers.clubs.nfl.com/nfl-assets/img/gbl-ico-team/GB/logos/home/large.png",
-					"img_visita" => "http://prod.static.49ers.clubs.nfl.com/nfl-assets/img/gbl-ico-team/SF/logos/home/large.png", 
-					"hora" => "1:00"
-				],
-				[
-					"fecha" => "2014-09-01",
-					"local" => "GB",
-					"visita" => "SF",
-					"img_local" => "http://prod.static.packers.clubs.nfl.com/nfl-assets/img/gbl-ico-team/GB/logos/home/large.png",
-					"img_visita" => "http://prod.static.49ers.clubs.nfl.com/nfl-assets/img/gbl-ico-team/SF/logos/home/large.png", 
-					"hora" => "1:00"
-				],
-				[
-					"fecha" => "2014-09-01",
-					"local" => "GB",
-					"visita" => "SF",
-					"img_local" => "http://prod.static.packers.clubs.nfl.com/nfl-assets/img/gbl-ico-team/GB/logos/home/large.png",
-					"img_visita" => "http://prod.static.49ers.clubs.nfl.com/nfl-assets/img/gbl-ico-team/SF/logos/home/large.png", 
-					"hora" => "1:00"
-				],
+		$url = "http://www.fantasyfootballnerd.com/service/schedule/xml/dv8qf84239ij/";
+		if (($response_xml_data = file_get_contents($url))===false){
+		    echo "Error fetching XML\n";
+		} else {
+		   libxml_use_internal_errors(true);
+		   $xml = simplexml_load_string($response_xml_data);
+		   if (!$xml) {
+		       echo "Error loading XML\n";
+		       foreach(libxml_get_errors() as $error) {
+		           echo "\t", $error->message;
+		       }
+		   } else {
+		   	  // echo "<pre>";
+		      // print_r($xml->Games->Game[0]['awayTeam']);
+		      // echo "</pre>";
+		   }
+		}
+		$data["partidos"] =[];
+		foreach ($xml->Games->Game as $game) {
+			$data["partidos"][] = 
+			[
+				"fecha" => $game['gameDate'],
+				"local" => $game['homeTeam'],
+				"visita" => $game['awayTeam'],
+				"img_local" => "http://prod.static.packers.clubs.nfl.com/nfl-assets/img/gbl-ico-team/".$game['homeTeam']."/logos/home/large.png",
+				"img_visita" => "http://prod.static.49ers.clubs.nfl.com/nfl-assets/img/gbl-ico-team/".$game['awayTeam']."/logos/home/large.png", 
+				"hora" => $game['gameTimeET']
 			];
+		}
+		
+
+		/*$data["partidos"] = [
+			[
+				"fecha" => "2014-09-01",
+				"local" => "GB",
+				"visita" => "SF",
+				"img_local" => "http://prod.static.packers.clubs.nfl.com/nfl-assets/img/gbl-ico-team/GB/logos/home/large.png",
+				"img_visita" => "http://prod.static.49ers.clubs.nfl.com/nfl-assets/img/gbl-ico-team/SF/logos/home/large.png", 
+				"hora" => "1:00"
+			],
+			[
+				"fecha" => "2014-09-01",
+				"local" => "GB",
+				"visita" => "SF",
+				"img_local" => "http://prod.static.packers.clubs.nfl.com/nfl-assets/img/gbl-ico-team/GB/logos/home/large.png",
+				"img_visita" => "http://prod.static.49ers.clubs.nfl.com/nfl-assets/img/gbl-ico-team/SF/logos/home/large.png", 
+				"hora" => "1:00"
+			],
+			[
+				"fecha" => "2014-09-01",
+				"local" => "GB",
+				"visita" => "SF",
+				"img_local" => "http://prod.static.packers.clubs.nfl.com/nfl-assets/img/gbl-ico-team/GB/logos/home/large.png",
+				"img_visita" => "http://prod.static.49ers.clubs.nfl.com/nfl-assets/img/gbl-ico-team/SF/logos/home/large.png", 
+				"hora" => "1:00"
+			],
+		];*/
 
 
 		return View::make("equipos.lista", $data);
